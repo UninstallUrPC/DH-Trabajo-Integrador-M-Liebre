a722222aa30534e50cdd0125efd77bf11b8f68b5
UninstallUrPC <echikup@hotmail.com>
2023-06-06 23:53:18 -0300

    primer commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.listen(3000, () => console.log("server on"));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, '/index.html'));
+});
