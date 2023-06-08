43f9c70fb77bcf82a6bdd620762a1978c447212a
UninstallUrPC <echikup@hotmail.com>
2023-06-07 23:43:37 -0300

    Cambios en el CSS, agregue el menu hamburguesa para pantallas mas chicas

--- app.js
+++ app.js
@@ -1,9 +1,16 @@
-const express = require("express");
-const path = require("path");
+const express = require('express');
+const path = require('path');
+
 const app = express();
+const port = 3000;
 
-app.listen(3000, () => console.log("server on"));
+// Serve static files from the same directory as app.js
+app.use(express.static(__dirname));
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, '/index.html'));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
 });
