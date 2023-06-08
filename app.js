f0e0650561422bcdb4b88ccaeb705dbb1ac6e59c
UninstallUrPC <echikup@hotmail.com>
2023-06-08 01:12:40 -0300

    Agrego login y signup, aunque contienen poco.

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-// Serve static files from the same directory as app.js
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
