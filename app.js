849050447d0596e7e63d6439057cbe01d461eff9
UninstallUrPC <echikup@hotmail.com>
2023-06-08 12:24:04 -0300

    Agregue la mayor parte de la funcionalidad de signup y login, cambios menores a home.

--- app.js
+++ app.js
@@ -7,7 +7,15 @@ const port = 3000;
 app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'index.html'));
+  res.sendFile(path.join(__dirname, 'home.html'));
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'login.html'));
+});
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'l signup.html'));
 });
 
 app.listen(port, () => {
